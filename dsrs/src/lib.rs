c95ea6071a78b255175c97588a3105268b69af41
Tiago de Paula <tiagodepalves@gmail.com>
2020-08-28 10:59:50 -0300

    Required features

--- dsrs/src/lib.rs
+++ dsrs/src/lib.rs
@@ -2,6 +2,14 @@
 #![feature(const_mut_refs)]
 #![feature(const_raw_ptr_deref)]
 #![feature(const_ptr_is_null)]
+#![feature(const_alloc_layout)]
+#![feature(const_size_of_val)]
+#![feature(const_align_of_val)]
+#![feature(const_result)]
+#![feature(const_panic)]
+#![feature(const_unreachable_unchecked)]
 #![feature(allocator_api)]
+#![feature(alloc_layout_extra)]
+#![feature(layout_for_ptr)]
 
 pub mod mem;
