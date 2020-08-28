3dd3e7587d380021bda1306b9f43e76e472ddd90
Tiago de Paula <tiagodepalves@gmail.com>
2020-08-27 21:21:58 -0300

    NonNull trait impls

--- dsrs/src/mem/nonnull.rs
+++ dsrs/src/mem/nonnull.rs
@@ -1,12 +1,16 @@
+use std::fmt::{Debug, Pointer, Formatter, Result};
+use std::hash::{Hash, Hasher};
+use std::cmp::Ordering;
+
+
 /// Wrapper for [`std::ptr::NonNull`]
 #[repr(transparent)]
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct NonNull<T: ?Sized>(pub std::ptr::NonNull<T>);
 
 
 impl<T: ?Sized> NonNull<T> {
     /// Read [`std::ptr::NonNull::new_unchecked`]
-    #[inline(always)]
+    #[inline]
     pub const unsafe fn new_unchecked(ptr: *mut T) -> Self {
         Self(std::ptr::NonNull::new_unchecked(ptr))
     }
@@ -16,7 +20,9 @@ impl<T: ?Sized> NonNull<T> {
     /// Since a valid reference is never null, this is always safe.
     /// This is also conceptually equivalent to `value as
     /// *const T *mut T`, which is safe and garanteed to be non null.
-    #[inline(always)]
+    ///
+    /// Note: implemented as method so that it can be `const`
+    #[inline]
     pub const fn from(value: &T) -> Self {
         let ptr = value as *const T as *mut T;
         // SAFETY: a reference is never null
@@ -24,9 +30,10 @@ impl<T: ?Sized> NonNull<T> {
     }
 
     /// Read [`std::ptr::NonNull::new`]
-    #[inline(always)]
+    #[inline]
     pub const fn new(ptr: *mut T) -> Option<Self> {
-        if !ptr.is_null() {
+        if ! ptr.is_null() {
+            // SAFETY: already checked for null
             Some(unsafe { Self::new_unchecked(ptr) })
         } else {
             None
@@ -34,33 +41,83 @@ impl<T: ?Sized> NonNull<T> {
     }
 
     /// Read [`std::ptr::NonNull::as_ptr`]
-    #[inline(always)]
-    pub const unsafe fn as_ptr(self) -> *mut T {
+    #[inline]
+    pub const fn as_ptr(self) -> *mut T {
         self.0.as_ptr()
     }
 
     /// Read [`std::ptr::NonNull::as_ref`]
-    #[inline(always)]
+    #[inline]
     pub const unsafe fn as_ref(&self) -> &T {
         &*(self.0.as_ptr() as *const T)
     }
 
     /// Read [`std::ptr::NonNull::as_mut`]
-    #[inline(always)]
+    #[inline]
     pub const unsafe fn as_mut(&mut self) -> &mut T {
         &mut *self.0.as_ptr()
     }
 
     /// Transmute `NonNull` into inner [`std::ptr::NonNull`]
-    #[inline(always)]
+    #[inline]
     pub const fn inner(self) -> std::ptr::NonNull<T> {
         self.0
     }
 
     /// Read [`std::ptr::NonNull::cast`]
-    #[inline(always)]
+    #[inline]
     pub const fn cast<U>(self) -> NonNull<U> {
         NonNull(self.0.cast())
+    }
+}
+
+impl<T: ?Sized> Clone for NonNull<T> {
+    #[inline]
+    fn clone(&self) -> Self {
+        Self(self.0)
+    }
+}
+impl<T: ?Sized> Copy for NonNull<T> {}
+
+impl<T: ?Sized> PartialEq for NonNull<T> {
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+impl<T: ?Sized> Eq for NonNull<T> {}
+
+impl<T: ?Sized> Ord for NonNull<T> {
+    #[inline]
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.cmp(&other.0)
+    }
+}
+impl<T: ?Sized> PartialOrd for NonNull<T> {
+    #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
+impl<T: ?Sized> Debug for NonNull<T> {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        Debug::fmt(&self.inner(), f)
+    }
+}
+
+impl<T: ?Sized> Pointer for NonNull<T> {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        Pointer::fmt(&self.inner(), f)
+    }
+}
+
+impl<T: ?Sized> Hash for NonNull<T> {
+    #[inline]
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.inner().hash(state)
     }
 }
 
