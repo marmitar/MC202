2d5c54d2260aec027f61d3d497a32c9cb8f772f3
Tiago de Paula <tiagodepalves@gmail.com>
2020-09-06 07:12:55 -0300

    Attribute checking

--- dsrs/repr_c_derive/src/derive.rs
+++ dsrs/repr_c_derive/src/derive.rs
@@ -2,7 +2,17 @@ use proc_macro2::TokenStream;
 use syn::DeriveInput;
 // use quote::quote;
 
+use crate::check::check_attributes;
 
-pub fn repr_c(_input: DeriveInput, _check: bool) -> TokenStream {
-    todo!()
+
+pub fn repr_c(input: DeriveInput, check: bool) -> TokenStream {
+    // TODO: check after?
+    if check {
+        // TODO: something else will return Results down there
+        if let Err(err) = check_attributes(input.attrs) {
+            return err.to_compile_error()
+        }
+    }
+
+    todo!();
 }
