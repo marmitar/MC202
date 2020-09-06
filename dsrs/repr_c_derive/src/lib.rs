2f0d7a69c9cb40b828fb68f022595d635fdd63a2
Tiago de Paula <tiagodepalves@gmail.com>
2020-09-06 07:43:25 -0300

    Woops...

--- dsrs/repr_c_derive/src/lib.rs
+++ dsrs/repr_c_derive/src/lib.rs
@@ -13,13 +13,13 @@ use derive::repr_c;
 
 
 #[proc_macro_derive(ReprC)]
-pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+pub fn rep_c_derive(input: TokenStream) -> TokenStream {
     let derive = parse_macro_input!(input);
     repr_c(derive, true).into()
 }
 
 #[proc_macro_derive(Unsafe_ReprC)]
-pub fn hello_macro_derive2(input: TokenStream) -> TokenStream {
+pub fn repr_c_unsafe_derive(input: TokenStream) -> TokenStream {
     let derive = parse_macro_input!(input);
     repr_c(derive, false).into()
 }
