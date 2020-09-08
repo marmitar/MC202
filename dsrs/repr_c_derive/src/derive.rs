09f46cb450700f53fcec4393db6b809c1e3e06b6
Tiago de Paula <tiagodepalves@gmail.com>
2020-09-08 18:35:19 -0300

    Derive function

--- dsrs/repr_c_derive/src/derive.rs
+++ dsrs/repr_c_derive/src/derive.rs
@@ -1,18 +1,75 @@
-use proc_macro2::TokenStream;
-use syn::DeriveInput;
-// use quote::quote;
+use proc_macro2::{Ident, Span, TokenStream};
+use syn::Data::{Enum, Struct, Union};
+use syn::{Attribute, DeriveInput, Error, Result};
 
-use crate::check::check_attributes;
+use quote::quote;
 
+/// Automatic trait to mark functions that can check attributes in
+/// [`DeriveInput`](DeriveInput#fields).
+///
+/// An [`AttrChecker`] function is responsible for checking the
+/// attributes of an input struct to guarantee that it follows
+/// a `#[repr(C)]` layout.
+///
+/// All other `repr` hints should cause `Err`. But take care, as
+/// repeated `C` hints are allowed in Rust. This should also `Err`
+/// if `#[repr(C)]` or equivalent is missing in attribute
+/// list.
+///
+/// # Note
+///
+/// This can't checke wether the input is a `struct` or not,
+/// but it should assume so.
+pub trait AttrChecker: FnOnce(Vec<Attribute>) -> Result<()> {}
+impl<F: FnOnce(Vec<Attribute>) -> Result<()>> AttrChecker for F {}
 
-pub fn repr_c(input: DeriveInput, check: bool) -> TokenStream {
-    // TODO: check after?
-    if check {
-        // TODO: something else will return Results down there
-        if let Err(err) = check_attributes(input.attrs) {
-            return err.to_compile_error()
+/// Implement [`ReprC`](mem::alloc::ReprC) for the struct in input
+/// `TokenStream`.
+///
+/// The [`attr_check`](AttrChecker) function should check the validity of the
+/// struct attributes, specially the `#[repr(...)]` attributes, as the
+/// `ReprC` trait can only be safely implemented on `#[repr(C)]` structs.
+///
+/// # Errors
+///
+/// This function will cause errors if `input` can't be parsed into a
+/// [`DeriveInput`] or if the type implementing `ReprC` isn't a
+/// [`struct`](Struct), as `enum`s and `union`s can't impl the trait.
+///
+/// This function will also return any errors encontered by `attr_check`.
+#[inline]
+pub fn impl_repr_c(input: TokenStream, attr_check: impl AttrChecker) -> Result<TokenStream> {
+
+    let DeriveInput { attrs, vis: _, ident: name, generics, data } = syn::parse2(input)?;
+
+    // get the type for the FieldTuple
+    let field = match data {
+        Struct(ref data) => data.fields.iter().map(|field| &field.ty),
+        Enum(_) => return Err(not_a_struct(&name, "enum")),
+        Union(_) => return Err(not_a_struct(&name, "union")),
+    };
+    // check after getting the fields, as that is a more important error
+    attr_check(attrs)?;
+
+    // the implementation is actually simple
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    Ok(quote! {
+        impl #impl_generics ::mem::alloc::ReprC for #name #ty_generics #where_clause {
+            type Fields = (#(#field,)*);
         }
-    }
+    })
+}
 
-    todo!();
+/// Generate standard error for when given type is not a struct.
+///
+/// `ident` should be the struct name and `data_type` is a
+/// string describing the kind of the type (`enum` or `union`).
+fn not_a_struct(ident: &Ident, data_type: &str) -> Error {
+    let message = format!(
+        "wrong type layout: 'ReprC' can only be \
+        implemented for structs, but {} is an {}",
+        ident, data_type
+    );
+
+    Error::new(Span::call_site(), message)
 }
--- dsrs/repr_c_derive/src/lib.rs
+++ dsrs/repr_c_derive/src/lib.rs
@@ -1,25 +1,35 @@
-#![feature(unsafe_block_in_unsafe_fn)]
-#![feature(iterator_fold_self)]
-#![feature(try_trait)]
-#![deny(unsafe_op_in_unsafe_fn)]
-
+#![feature(const_option)]
+#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
+#![allow(clippy::cargo_common_metadata)]
+#![allow(clippy::multiple_crate_versions)]
+#![allow(clippy::wildcard_dependencies)]
 extern crate proc_macro;
+
 mod check;
 mod derive;
 
 use proc_macro::TokenStream;
-use syn::parse_macro_input;
-use derive::repr_c;
 
+use check::check_attributes;
+use derive::impl_repr_c;
 
 #[proc_macro_derive(ReprC)]
-pub fn rep_c_derive(input: TokenStream) -> TokenStream {
-    let derive = parse_macro_input!(input);
-    repr_c(derive, true).into()
+pub fn repr_c_derive(input: TokenStream) -> TokenStream {
+
+    match impl_repr_c(input.into(), check_attributes) {
+        Ok(derive) => derive,
+        Err(err) => err.to_compile_error(),
+    }
+    .into()
 }
 
 #[proc_macro_derive(Unsafe_ReprC)]
 pub fn repr_c_unsafe_derive(input: TokenStream) -> TokenStream {
-    let derive = parse_macro_input!(input);
-    repr_c(derive, false).into()
+    let skip_check = |_| Ok(());
+
+    match impl_repr_c(input.into(), skip_check) {
+        Ok(derive) => derive,
+        Err(err) => err.to_compile_error(),
+    }
+    .into()
 }
